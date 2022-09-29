5748bf1751513b428389e529e38684ac907e9583
JORGE Jonathan <tm6276@equans.com>
2022-09-29 15:37:18 +0200

    (update) Ajout des commentaires script1

--- script1.py
+++ script1.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 
+#Les variables à initialiser 
 min = 1
 max = 100000
 
-
+#Boucle qui permettant de ressortir les nombres premiers de 1 à 100 000
 for n in range(min,max + 1):
+
+#Si n est uniquement superieure à 1
    if n > 1:
+#boucle qui test si il s'agit d'un nombre premier
        for i in range(2,n):
            if (n % i) == 0:
                break
        else:
+#Affiche le nombre premier
            print(n)
